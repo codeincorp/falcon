f7942e71851cb3a19a421e4fedb624eea420fb62
Youngjoon Kim <buffterran@codeinco.io>
2021-02-24 10:00:15 -0800

    modified iostream.cpp

--- src/iostream.cpp
+++ src/iostream.cpp
@@ -4,21 +4,55 @@
 #include <cassert>
 #include <string>
 #include <error.h>
+#include <vector>
 
 using namespace std;
 
-int main() {
+vector<string> parse_line(const string& line) {
+    int left = -1;
+    int right = 0;
+    int len = line.length();
+    int i = 0;
+    vector<string> strs;
+
+    while(i <= len) {
+        if( i == len || line.at(i) == ','){
+          strs.emplace_back(line.substr(left, right-left));  
+          left = -1;      
+        }
+        else if (line.at(i) != ' '){
+          if (left == -1){
+            left = i;
+          }
+          right = i+1;
+        }
+        
+        i++;
+    }
+    return strs;
+}
+
+int main(string fileName) {
   //file name
-    string name = "list1.txt";
+    string name = fileName;
     //string name1 = "list.txt";
 
     fstream ost;
    
     ost.open(name, fstream::in);
-  
+
     string reading;
-    while(!getline(ost, reading,',').eof()) {
-        cout << reading << endl;
+    vector<string> fields;
+    int i = 0;
+
+    while(!getline(ost, reading).eof()) {
+        cout << "line " << i << ":";
+        fields = parse_line(reading);
+        for( int k = 0; k < fields.size(); k++) {
+          cout << fields[k] + ",";
+        }
+        cout << endl; 
+        i++;
     }
     
     ost.close();
