dcaec47d3832fa29e46049f7ad523f43e81315a6
Codein Developer <dev@codeinco.io>
2021-02-23 23:06:10 -0800

    To test debugging facilities

--- src/opt_n_any.cpp
+++ src/opt_n_any.cpp
@@ -6,38 +6,34 @@
 #include <typeindex>
 #include <typeinfo>
 #include <unordered_map>
+#include <set>
 #include <vector>
+#include <string>
+#include <list>
+#include <array>
+#include <memory>
  
 template<class T, class F>
-inline std::pair<const std::type_index, std::function<void(std::any const&)>>
+constexpr std::pair<const std::type_index, std::function<void(std::any const&)>>
     to_any_visitor(F const &f)
 {
     return {
         std::type_index(typeid(T)),
         [g = f](std::any const &a)
         {
-            if constexpr (std::is_void_v<T>)
+            if constexpr (std::is_void_v<T>) {
                 g();
-            else
+            } else {
                 g(std::any_cast<T const&>(a));
+            }
         }
     };
 }
  
-static std::unordered_map<
-    std::type_index, std::function<void(std::any const&)>>
-    any_visitor {
-        to_any_visitor<void>([]{ std::cout << "{}"; }),
-        to_any_visitor<int>([](int x){ std::cout << x; }),
-        to_any_visitor<unsigned>([](unsigned x){ std::cout << x; }),
-        to_any_visitor<float>([](float x){ std::cout << x; }),
-        to_any_visitor<double>([](double x){ std::cout << x; }),
-        to_any_visitor<char const*>([](char const *s)
-            { std::cout << std::quoted(s); }),
-        // ... add more handlers for your types ...
-    };
- 
-inline void process(const std::any& a)
+using any_visitor_map = std::unordered_map<
+    std::type_index, std::function<void(std::any const&)>>;
+
+inline void process(const any_visitor_map& any_visitor, const std::any& a)
 {
     if (const auto it = any_visitor.find(std::type_index(a.type()));
         it != any_visitor.cend()) {
@@ -48,47 +44,84 @@ inline void process(const std::any& a)
 }
  
 template<class T, class F>
-    inline void register_any_visitor(F const& f)
+    inline void register_any_visitor(any_visitor_map& any_visitor, F const& f)
 {
     std::cout << "Register visitor for type "
               << std::quoted(typeid(T).name()) << '\n';
     any_visitor.insert(to_any_visitor<T>(f));
 }
  
+struct my_type {
+    std::string s;
+    std::vector<int> vi;
+};
+
 auto main() -> int
 {
-    std::vector<std::any> va { {}, 42, 123u, 3.14159f, 2.71828, "C++17", };
+    static any_visitor_map any_visitor {
+        to_any_visitor<void>([]{ std::cout << "{}"; }),
+        to_any_visitor<int>([](int x){ std::cout << x; }),
+        to_any_visitor<unsigned>([](unsigned x){ std::cout << x; }),
+        to_any_visitor<float>([](float x){ std::cout << x; }),
+        to_any_visitor<double>([](double x){ std::cout << x; }),
+        to_any_visitor<char const*>([](char const *s)
+            { std::cout << std::quoted(s); }),
+        to_any_visitor<std::string>([](std::string x){ std::cout << std::quoted(x); })
+        // ... add more handlers for your types ...
+    };
+ 
+    std::vector<std::any> va { {}, 42, 123u, 3.14159f, 2.71828, "C++17", std::string("C++20")};
  
     std::cout << "{ ";
     for (const std::any& a : va) {
-        process(a);
+        process(any_visitor, a);
         std::cout << ", ";
     }
     std::cout << "}\n";
  
-    process(std::any(0xFULL)); //< Unregistered type "y" (unsigned long long)
+    process(any_visitor, std::any(0xFULL)); //< Unregistered type "y" (unsigned long long)
     std::cout << '\n';
  
-    register_any_visitor<unsigned long long>([](auto x) {
+    register_any_visitor<unsigned long long>(any_visitor, [](auto x) {
         std::cout << std::hex << std::showbase << x; 
         std::cout << std::dec << std::noshowbase;
     });
  
-    process(std::any(0xFULL)); //< OK: 0xf
+    process(any_visitor, std::any(0xFULL)); //< OK: 0xf
     std::cout << '\n';
 
-    std::optional<std::string> opt_str = "abc";
+    std::optional opt_str = "abc";
 
     std::cout << opt_str.value() << std::endl;
 
     std::optional<std::vector<std::any>> opt_vec = va;
     std::cout << "{ ";
     for (const std::any& a : opt_vec.value()) {
-        process(a);
+        process(any_visitor, a);
         std::cout << ", ";
     }
     std::cout << "}\n";
 
     std::optional<int> opt_int = std::nullopt;
     std::cout << opt_int.value_or(-1) << std::endl;
+
+    std::unordered_map<std::string, int> int_map{{"1",1}, {"2",2}};
+
+    std::set int_set{1,2,3,4};
+
+    std::string s{"std::string"};
+
+    std::list l{1,2,3};
+
+    std::optional<std::string> opt = std::nullopt;
+
+    my_type mt{"ABCD", {3,2,1}};
+
+    std::array arr{1,2,3,4,5};
+
+    auto p = std::make_unique<my_type>(std::string{"EFGH"}, std::vector{4,5,6});
+
+    auto p1 = std::make_shared<my_type>(std::string{"EFGH"}, std::vector{4,5,6});
+
+    return 0;
 }
