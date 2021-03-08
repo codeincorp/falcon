1958c015bc27dd266235f627014327bc19e6a8b6
Codein Developer <dev@codeinco.io>
2021-03-07 21:14:43 -0800

    #66 Improved Metadata design for the faster field lookup

--- src/metadata.h
+++ src/metadata.h
@@ -1,6 +1,8 @@
+#include <initializer_list>
 #include <string>
 #include <typeindex>
 #include <vector>
+#include <unordered_map>
 
 #include "to_any_converter.h"
 
@@ -20,6 +22,11 @@ struct FieldMetaInfo {
         , typeIndex(typeIndex)
     {}
 
+    FieldMetaInfo(const char* fieldName, std::type_index typeIndex)
+        : fieldName(fieldName)
+        , typeIndex(typeIndex)
+    {}
+
     FieldMetaInfo()
         : FieldMetaInfo("", tiVoid)
     {}
@@ -28,6 +35,67 @@ struct FieldMetaInfo {
 /**
  * @brief Metadata information about data
  */
-using Metadata = std::vector<FieldMetaInfo>;
+class Metadata {
+public:
+    Metadata() = default;
+    Metadata(const Metadata&) = default;
+    Metadata(Metadata&&) = default;
+
+    Metadata(std::initializer_list<FieldMetaInfo> il)
+        : fields_(il)
+        , colToIdx_()
+    {}
+
+    explicit Metadata(std::size_t sz)
+        : fields_(sz)
+        , colToIdx_()
+    {}
+
+    Metadata& operator=(const Metadata&) = default;
+    Metadata& operator=(Metadata&&) = default;
+
+    const FieldMetaInfo& operator[](std::size_t i) const {
+        return fields_[i];
+    }
+
+    FieldMetaInfo& operator[](std::size_t i) {
+        return fields_[i];
+    }
+
+    std::size_t operator[](const std::string& name) const {
+        ensureColToIdxSetup();
+        return colToIdx_[name];
+    }
+
+    std::size_t size() const {
+        return fields_.size();
+    }
+
+    void reserve(std::size_t sz) {
+        fields_.reserve(sz);
+    }
+
+    template <typename... ArgTs>
+    Metadata& emplace_back(ArgTs&&... args)
+    {
+        fields_.emplace_back(std::forward<ArgTs>(args)...);
+        return *this;
+    }
+
+private:
+    void ensureColToIdxSetup() const {
+        if (fields_.size() == colToIdx_.size()) {
+            return;
+        }
+
+        for (size_t i = 0; i < fields_.size(); ++i) {
+            const auto& fieldName = fields_[i].fieldName;
+            colToIdx_[fieldName] = i;
+        }
+    }
+
+    std::vector<FieldMetaInfo> fields_;
+    mutable std::unordered_map<std::string, std::size_t> colToIdx_;
+};
 
 }
